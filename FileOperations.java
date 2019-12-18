2e1a62ac6c1bbc90586e6c732d9d9f2b41558d8d
Yasir777 <45829991+Yasir77788@users.noreply.github.com>
2019-12-18 03:33:11 -0600

    Add files via upload

---
+++ FileOperations.java
@@ -0,0 +1,54 @@
+// Feb 19, 2019 - Yasir Hassan
+// CS 3300
+// Assignment on inputFile operations
+
+package fileoperations;
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+
+public class FileOperations 
+{
+    // Class attributes
+    private final String filePath;
+    private Scanner inputFile;
+    
+    // Constructor to initialize class attributes
+    public FileOperations(String filePath)
+    {
+        this.filePath = filePath;
+        inputFile = null;
+    }
+    
+    // opnen file method
+    public void openFile()
+    {
+        try
+        {
+            inputFile = new Scanner(new FileInputStream(filePath));
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println("Error - file was not found.");
+            System.exit(0);
+        }
+    }
+    
+    // Read file and display its contents
+    public void readFile()
+    {
+        String line;
+        while(inputFile.hasNext())
+        {
+            line = inputFile.nextLine();
+            System.out.println(line);
+        }
+    }
+    
+    // close file
+    public void closeFile()
+    {
+        inputFile.close();
+    }
+}
---
+++ MainClass.java
@@ -0,0 +1,39 @@
+// Feb 19, 2019 - Yasir Hassan
+// CS 3300
+// Assignment on inputFile operations
+
+package fileoperations;
+import java.util.Scanner;
+
+public class MainClass 
+{
+    // main-method gets the user input
+    // creates a FileOperations object,
+    // invokes FileOperations methods
+    public static void main(String[] args)
+    {
+        String filePath;
+        
+        // create a Scanner object
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter file path to be read: ");
+        filePath = input.nextLine();
+        
+        // Creat file object and pass the file path/name to the constructor
+        FileOperations fileObj = new FileOperations(filePath);
+        
+        // performing operations on file:
+        System.out.println("\nOpenning the file...");
+        
+        // Open the file
+        fileObj.openFile();
+        
+        // read file and display its contents
+        System.out.println("Reading the file and displaying its contents:");
+        System.out.println("=============================================");
+        fileObj.readFile();
+        
+        // close the file
+        fileObj.closeFile();
+    }
+}
